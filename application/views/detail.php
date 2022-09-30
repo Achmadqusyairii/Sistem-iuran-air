d00a01db40c52f6e898c80cac5b725c19493b202
achmadqusyairii <achmadqusayiri291017@gmail.com>
2022-09-30 19:56:33 +0700

    first commit

---
+++ application/views/detail.php
@@ -0,0 +1,24 @@
+
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">PROFILE ANGGOTA</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <p class="card-text">kode provinsi :<?php echo $detail->kd_prov ?></p>
+        <p class="card-text">kode kabupaten :<?php echo $detail->kd_kab ?><br></p>
+        <p class="card-text">kode kecamatan :<?php echo $detail->kd_kec ?><br></p>
+        <p class="card-text">kode desa :<?php echo $detail->kd_desa ?></p>
+        <p class="card-text">kode level :<?php echo $detail->level ?></p>
+        <p class="card-text">kode ttl :<?php echo $detail->tgl_lahir ?></p>
+        <p class="card-text">kode nik :<?php echo $detail->nik ?></p>
+        <p class="card-text">kode no kta :<?php echo $detail->no_kta ?></p>
+        <p class="card-text">kode alamat :<?php echo $detail->alamat ?></p>
+      </div>
+      </div>
+      </div>
+    </div>
+  </div>
